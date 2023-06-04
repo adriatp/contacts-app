1d7737c7f651bff92397f66c7690a74f28fab725
adriatp <tarradas.adria@gmail.com>
2023-06-04 20:58:53 +0200

    Create fixtures and set seeds with them

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-(1..20).each do |i|
-  c = Contact.create(
-    firstname: Faker::Name.first_name,
-    lastname: Faker::Name.last_name,
-    phone: Faker::PhoneNumber.cell_phone 
-  )
-  puts "Created contact with id=#{c.id}"
-end
+# (1..20).each do |i|
+#   c = Contact.create(
+#     firstname: Faker::Name.first_name,
+#     lastname: Faker::Name.last_name,
+#     phone: Faker::PhoneNumber.cell_phone 
+#   )
+#   puts "Created contact with id=#{c.id}"
+# end
+
+puts "\n== Seeding the database with fixtures =="
+system("bin/rails db:fixtures:load")
