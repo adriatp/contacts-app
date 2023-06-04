3dd452b5bb6b3c6084c7938f2d1e57925aad7901
adriatp <tarradas.adria@gmail.com>
2023-06-04 20:57:22 +0200

    Validations, turbo_methods, ransack attributes...

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,12 @@
 class Contact < ApplicationRecord
-  after_create_commit { broadcast_prepend_to "contacts" }
-  after_update_commit { broadcast_replace_to "contacts" }
-  after_destroy_commit { broadcast_remove_to "contacts" }
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :phone, presence: true
 
+  after_create_commit -> { broadcast_prepend_later_to "contacts" }
+  after_update_commit -> { broadcast_replace_later_to "contacts" }
+  after_destroy_commit -> { broadcast_remove_to "contacts" }
+  
   paginates_per 10
 
   def self.ransackable_attributes(auth_object = nil)
