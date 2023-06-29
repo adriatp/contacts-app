15786fa5112fd90d2ab3e5c0071bfccc9f53fefa
adriatp <tarradas.adria@gmail.com>
2023-06-29 20:38:11 +0200

    Add contact controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ContactController from "./contacts_controller"
+application.register("delete", ContactController)
