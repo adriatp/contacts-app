604db4c030255a3226fdff92ed656143d7e2542e
adriatp <tarradas.adria@gmail.com>
2023-06-29 18:46:20 +0200

    Populate db with contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..20).each do |i|
+  c = Contact.create(
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    phone: Faker::PhoneNumber.cell_phone 
+  )
+  puts "Created contact with id=#{c.id}"
+end
