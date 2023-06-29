66718423dd0188e58e4fd7113e4b9d0869f6b0e1
adriatp <tarradas.adria@gmail.com>
2023-06-29 21:18:46 +0200

    Create with stimulus creator

--- app/javascript/controllers/contacts_controller.js
+++ app/javascript/controllers/contacts_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="contacts"
 export default class extends Controller {
   delete(event) {
     let confirmed = confirm("Are you sure?")
