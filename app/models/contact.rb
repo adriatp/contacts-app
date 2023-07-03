79047329aa6f8078267f76e81b206cf7f26a7595
adriatp <tarradas.adria@gmail.com>
2023-07-03 23:43:26 +0200

    Update list on crud

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ApplicationRecord
+  after_create_commit { broadcast_prepend_to "contacts" }
+  after_update_commit { broadcast_replace_to "contacts" }
+  after_destroy_commit { broadcast_remove_to "contacts" }
+
   paginates_per 10
 
   def self.ransackable_attributes(auth_object = nil)
