7ac7d4590983f7e2ab3954bdbeff5a4639a85733
adria.tp <adria.tp@coditramuntana.com>
2023-07-05 07:42:34 +0200

    Updated system tests

--- test/system/contacts_test.rb
+++ test/system/contacts_test.rb
@@ -7,33 +7,69 @@ class ContactsTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit contacts_url
-    assert_selector "h1", text: "Contacts"
+    assert_selector "h2", text: "Contacts"
   end
 
   test "should create contact" do
     visit contacts_url
-    click_on "New contact"
-
+    
+    click_on "+ Add contact"
+    
+    fill_in "Firstname", with: Faker::Name.first_name
+    fill_in "Lastname", with: Faker::Name.last_name
+    fill_in "Phone", with: Faker::PhoneNumber.cell_phone
+    
     click_on "Create Contact"
 
     assert_text "Contact was successfully created"
-    click_on "Back"
+  end
+
+  test "should say that attributes are missing on create" do
+    visit contacts_url
+    
+    click_on "+ Add contact"
+    
+    click_on "Create Contact"
+
+    assert_text "Firstname can't be blank"
+    assert_text "Lastname can't be blank"
+    assert_text "Phone can't be blank"
   end
 
   test "should update Contact" do
-    visit contact_url(@contact)
-    click_on "Edit this contact", match: :first
+    visit contacts_url
+    click_on(class: 'contact-link', match: :first)
+
+    fill_in "Firstname", with: Faker::Name.first_name
+    fill_in "Lastname", with: Faker::Name.last_name
+    fill_in "Phone", with: Faker::PhoneNumber.cell_phone
 
     click_on "Update Contact"
 
     assert_text "Contact was successfully updated"
-    click_on "Back"
+  end
+
+  test "should say that attributes are missing on update" do
+    visit contacts_url
+
+    click_on(class: 'contact-link', match: :first)
+
+    fill_in "Firstname", with: ""
+    fill_in "Lastname", with: ""
+    fill_in "Phone", with: ""
+
+    click_on "Update Contact"
+
+    assert_text "Firstname can't be blank"
+    assert_text "Lastname can't be blank"
+    assert_text "Phone can't be blank"
   end
 
   test "should destroy Contact" do
-    visit contact_url(@contact)
-    click_on "Destroy this contact", match: :first
-
+    visit contacts_url
+    accept_alert do
+      click_on(class: 'btn-danger', match: :first)
+    end
     assert_text "Contact was successfully destroyed"
   end
 end
